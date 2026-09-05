bbc2fbaea969820ee42e479ac79f79c1b1c4a5c7
issacawuah <email>
2026-09-05 10:42:23 +0000

    Add categories page and update site

--- server.js
+++ server.js
@@ -49,6 +49,12 @@ app.get('/projects', async (req, res) => {
     res.render('projects', { title });
 });
 
+
+app.get('/categories', async (req, res) => {
+    const title = 'Service Project Categories';
+    res.render('categories', { title });
+});
+
 /**
  * Start the server
  */

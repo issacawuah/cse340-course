f595951e263a506fa2bc93255ffce3772b9b73e6
issacawuah <email>
2026-09-12 16:09:09 +0000

    Connect to database and add organizations route

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
+import 'dotenv/config';
 import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import { testConnection } from './cse340-course/src/models/db.js';
+import { getAllOrganizations } from './src/models/organizations.js';
 
+console.log('DATABASE_URL loaded:', process.env.DATABASE_URL ? process.env.DATABASE_URL.substring(0, 30) + '...' : 'UNDEFINED');
+
+const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+
 // Define the application environment
-const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
+
+
 
 // Define the port number the server will listen on
 const PORT = process.env.PORT || 3000;
@@ -19,15 +27,17 @@ const app = express();
 
 
 // Set EJS as the templating engine
+app.use(express.static(path.join(__dirname, 'cse340-course/public')));
 app.set('view engine', 'ejs');
 
 // Tell Express where to find your templates
-app.set('views', path.join(__dirname, 'src/views'));
+app.set('views', path.join(__dirname, 'cse340-course/src/views'));
+console.log('VIEWS DIRECTORY:', app.get('views'));
 
 
 
 // Serve static files from the public directory
-app.use(express.static(path.join(__dirname, 'public')));
+
 
 
 
@@ -40,6 +50,9 @@ app.get('/', async (req, res) => {
 });
 
 app.get('/organizations', async (req, res) => {
+    const organizations = await getAllOrganizations();
+    console.log(organizations);
+
     const title = 'Our Partner Organizations';
     res.render('organizations', { title });
 });
@@ -58,7 +71,12 @@ app.get('/categories', async (req, res) => {
 /**
  * Start the server
  */
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, async () => {
+    try {
+        await testConnection();
+        console.log(`Server is running at http://127.0.0.1:${PORT}`);
+        console.log(`Environment: ${NODE_ENV}`);
+    } catch (error) {
+        console.error('Error connecting to the database:', error);
+    }
 });

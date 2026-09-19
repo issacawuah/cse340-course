29169ce49874bfe4aa65c00e23e1709e15dec537
issacawuah <email>
2026-09-19 15:19:46 +0000

    Add category details page and category tags on service project pages

--- src/models/categories.js
+++ src/models/categories.js
@@ -11,4 +11,38 @@ const getAllCategories = async () => {
     return result.rows;
 };
 
-export { getAllCategories };
+const getCategoryDetails = async (categoryId) => {
+    const query = `
+        SELECT category_id, name
+        FROM category
+        WHERE category_id = $1;
+    `;
+
+    const queryParams = [categoryId];
+    const result = await db.query(query, queryParams);
+
+    return result.rows.length > 0 ? result.rows[0] : null;
+};
+
+const getProjectsByCategoryId = async (categoryId) => {
+    const query = `
+        SELECT
+            project.project_id,
+            project.title,
+            project.description,
+            project.location,
+            project.project_date
+        FROM project
+        JOIN project_category ON project.project_id = project_category.project_id
+        WHERE project_category.category_id = $1
+        ORDER BY project.project_date;
+    `;
+
+    const queryParams = [categoryId];
+    const result = await db.query(query, queryParams);
+
+    return result.rows;
+};
+
+// Export the model functions
+export { getAllCategories, getCategoryDetails, getProjectsByCategoryId };
--- src/models/projects.js
+++ src/models/projects.js
@@ -77,5 +77,21 @@ const getProjectDetails = async (id) => {
     return result.rows.length > 0 ? result.rows[0] : null;
 };
 
+const getCategoriesByProjectId = async (projectId) => {
+    const query = `
+        SELECT
+            category.category_id,
+            category.name
+        FROM category
+        JOIN project_category ON category.category_id = project_category.category_id
+        WHERE project_category.project_id = $1;
+    `;
+
+    const queryParams = [projectId];
+    const result = await db.query(query, queryParams);
+
+    return result.rows;
+};
+
 // Export the model functions
-export { getAllProjects, getProjectsByOrganizationId, getUpcomingProjects, getProjectDetails };
+export { getAllProjects, getProjectsByOrganizationId, getUpcomingProjects, getProjectDetails, getCategoriesByProjectId };
